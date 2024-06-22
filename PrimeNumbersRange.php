53f4288978f55a0c4ec26b464ee2854f5fa05a19
MURTADHA ALBAHRANI <160097883+MURTADHA28@users.noreply.github.com>
2024-06-22 10:40:21 +0300

    Update PrimeNumbersRange.php

--- PrimeNumbersRange.php
+++ PrimeNumbersRange.php
@@ -19,3 +19,5 @@ $startRange = 90;
 $endRange = 200;
 $primeList = getPrimesBetween($startRange, $endRange);
 echo implode(', ', $primeList);
+
+        //OUTPUT :97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199
